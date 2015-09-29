473b6b449e6e65f6ffde24f48241e362181ae5c5
macknilan <rodolfougaldeochoa@gmail.com>
2015-09-28 21:01:11 -0500

    Mod. de plantillas para mostrar imagenes 2

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -234,7 +234,7 @@ CKEDITOR_CONFIGS = {
             ['Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates'],
             ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'],
             ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt'],
-            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+#            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
             ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
             ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
                 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl'],
