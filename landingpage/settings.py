e8691d4fa221e43af74bfd971af08b2fe51e538f
macknilan <rodolfougaldeochoa@gmail.com>
2015-09-30 23:35:06 -0500

    MOD MAYOR PARA CAMBIAR TEMPLATE A materializecss - FALTA FORMULARIO DE CONTACTO Y MAPA

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['MUEBLERIALLAVE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '52.8.45.246', 'muebleria.konetl.co']
 # ALLOWED_HOSTS = ['*']
