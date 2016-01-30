2648c8e7615963d57ad6c887836e0dd7625a61ba
macknilan <rodolfougaldeochoa@gmail.com>
2016-01-29 22:58:29 -0600

    Mapa de Sitio 1

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['MUEBLERIALLAVE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '52.8.119.177']
 # ALLOWED_HOSTS = ['*']
